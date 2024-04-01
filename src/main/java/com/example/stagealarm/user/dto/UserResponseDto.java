43a9e3bfdcce9c59436b50aa68c4acd9cfe27355
KwonHyeokGeon <weekend12@icloud.com>
2024-04-01 13:59:08 +0900

    Fix: update show.html 1. 페이지네이션 업데이트 2. 댓글 업데이트 삭제 기능 구현

--- src/main/java/com/example/stagealarm/user/dto/UserResponseDto.java
+++ src/main/java/com/example/stagealarm/user/dto/UserResponseDto.java
@@ -13,12 +13,14 @@ public class UserResponseDto {
     // comments에 get요청이 왔을 때 user정보를 주기 위한 userDto(userEntity를 직접 넘기지 않기 위해)
     private String nickname;
     private Long userId;
+    private String authorities;
 
     public static UserResponseDto fromEntity(UserEntity user) {
         if (user == null) return null;
         return UserResponseDto.builder()
                 .nickname(user.getNickname())
                 .userId(user.getId())
+                .authorities(user.getAuthorities())
                 .build();
     }
 }
